783b7a9ddafc351f61586b1da0dac834b64bfec3
Michael_j_nj <georgejosin6@gmail.com>
2025-10-16 22:08:47 +0530

    Done ID,EX,MEM,WB stages

--- mips_pipe.v
+++ mips_pipe.v
@@ -16,9 +16,9 @@ parameter RR_ALU=3'b000, RM_ALU=3'b001, LOAD=3'b010, STORE=3'b011, BRANCH=3'b100
 
 
 reg HALTED;
-     //Set after HLT instruction is completed(in WB stage)
+//Set after HLT instruction is completed(in WB stage)
 reg TAKEN_BRANCH;
-     //Required to disable instructions after branch
+//Required to disable instructions after branch
 
 //IF Stage
 always @(posedge clk1)                                  // IF Stage
@@ -38,9 +38,119 @@ begin
           PC            <= #2 PC + 1;
      end
 end
+//ID Stage 
+always @(posedge clk2)                                  // ID Stage
+if (HALTED == 0)
+begin
+          if (IF_ID_IR[25:21] == 5'b00000)  
+               ID_EX_A   <= 0;
+          else 
+               ID_EX_A   <= #2 Reg[IF_ID_IR[25:21]]; // "rs"
+
+          if (IF_ID_IR[20:16] == 5'b00000)  
+               ID_EX_B   <= 0;
+          else 
+               ID_EX_B   <= #2 Reg[IF_ID_IR[20:16]]; // "rt"
+
+          ID_EX_NPC      <= #2 IF_ID_NPC;
+          ID_EX_IR       <= #2 IF_ID_IR;
+          ID_EX_Imm      <= #2 {{16{IF_ID_IR[15]}}, {IF_ID_IR[15:0]}};
+     
+
+     case (IF_ID_IR[31:26])
+          ADD,SUB,AND,OR,SLT,MUL: ID_EX_type <= #2 RR_ALU;
+          ADDI,SUBI,SLTI:         ID_EX_type <= #2 RM_ALU;
+          LW:                     ID_EX_type <= #2 LOAD;
+          SW:                     ID_EX_type <= #2 STORE;
+          BNEQZ,BEQZ:             ID_EX_type <= #2 BRANCH;
+          HLT:                    ID_EX_type <= #2 HALT;
+          default:                ID_EX_type <= #2 HALT;// Invalid opcode
+     endcase
+end
 
 
+always @(posedge clk1)// EX Stage
+if (HALTED == 0)
+begin
+  EX_MEM_type   <= #2 ID_EX_type;
+  EX_MEM_IR     <= #2 ID_EX_IR;
+  TAKEN_BRANCH  <= #2 0;
+
+  case (ID_EX_type)
+     RR_ALU: 
+     begin
+          case (ID_EX_IR[31:26])// "opcode"
+               ADD:    EX_MEM_ALUout <= #2 ID_EX_A + ID_EX_B;
+               SUB:    EX_MEM_ALUout <= #2 ID_EX_A - ID_EX_B;
+               AND:    EX_MEM_ALUout <= #2 ID_EX_A & ID_EX_B;
+               OR:     EX_MEM_ALUout <= #2 ID_EX_A | ID_EX_B;
+               SLT:    EX_MEM_ALUout <= #2 ID_EX_A < ID_EX_B;
+               MUL:    EX_MEM_ALUout <= #2 ID_EX_A * ID_EX_B;
+               default: EX_MEM_ALUout <= #2 32'hxxxxxxxx;
+          endcase
+    end
+     RM_ALU: 
+     begin
+          case (ID_EX_IR[31:26])// "opcode"
+               ADDI:    EX_MEM_ALUout <= #2 ID_EX_A + ID_EX_Imm;
+               SUBI:    EX_MEM_ALUout <= #2 ID_EX_A - ID_EX_Imm;
+               SLTI:    EX_MEM_ALUout <= #2 ID_EX_A < ID_EX_Imm;
+               default: EX_MEM_ALUout <= #2 32'hxxxxxxxx;
+          endcase
+     end
+
+     LOAD, STORE:
+     begin
+          EX_MEM_ALUout <= #2 ID_EX_A + ID_EX_Imm;
+          EX_MEM_B      <= #2 ID_EX_B;
+     end
+
+     BRANCH: 
+     begin
+          EX_MEM_ALUout <= #2 ID_EX_NPC + ID_EX_Imm;
+          EX_MEM_cond   <= #2 (ID_EX_A == 0);
+     end
+
+endcase
+end
+   
+always @(posedge clk2)// MEM Stage
+if (HALTED == 0)
+begin
+  MEM_WB_type       <= #2 EX_MEM_type;
+  MEM_WB_IR         <= #2 EX_MEM_IR;
+
+  case (EX_MEM_type)
+     RR_ALU, RM_ALU:
+          MEM_WB_ALUout       <= #2 EX_MEM_ALUout;
+
+     LOAD:
+          MEM_WB_LMD          <= #2 Mem[EX_MEM_ALUout];
+
+     STORE:
+      if (TAKEN_BRANCH == 0)// Disable write
+        Mem[EX_MEM_ALUout]    <= #2 EX_MEM_B;
+
+  endcase
+end
 
 
+always @(posedge clk1)// WB Stage
+begin
+  if (TAKEN_BRANCH == 0)// Disable write if branch taken
+    case (MEM_WB_type)
+      RR_ALU:   
+          Reg[MEM_WB_IR[15:11]]    <= #2 MEM_WB_ALUout;// "rd"
+      RM_ALU:   
+          Reg[MEM_WB_IR[20:16]]    <= #2 MEM_WB_ALUout;// "rt"
+      LOAD:     
+          Reg[MEM_WB_IR[20:16]]    <= #2 MEM_WB_LMD;// "rt"
+      HALT:     
+          HALTED <= #2 1'b1;
+    endcase
+end
 
 endmodule
+
+
+
