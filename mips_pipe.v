6e3e6ad50df43b0343fa4b6f8d0aaaa309de6c69
Michael_j_nj <georgejosin6@gmail.com>
2025-10-18 15:59:17 +0530

    completed pipeline design and two testbench

--- mips_pipe.v
+++ mips_pipe.v
@@ -4,7 +4,7 @@ module pipe_MIPS32 (clk1,clk2);
 reg [31:0] PC,IF_ID_IR,IF_ID_NPC;
 reg [31:0] ID_EX_IR,ID_EX_NPC,ID_EX_A,ID_EX_B,ID_EX_Imm;
 reg [2:0] ID_EX_type,EX_MEM_type,MEM_WB_type;
-reg [31:0] EX_MEM_IR,EX_MEM_ALUout,EX_MEM_B
+reg [31:0] EX_MEM_IR,EX_MEM_ALUout,EX_MEM_B;
 reg        EX_MEM_cond;
 reg [31:0] MEM_WB_IR,MEM_WB_ALUout,MEM_WB_LMD;
 
